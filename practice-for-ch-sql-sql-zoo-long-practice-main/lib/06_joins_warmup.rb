2a4766d91f1057f6ff242fcdbf25a41f96289b66
AkashPulluru <akashpulluru@gmail.com>
2023-07-25 16:05:20 -0400

    part 6 done

--- practice-for-ch-sql-sql-zoo-long-practice-main/lib/06_joins_warmup.rb
+++ practice-for-ch-sql-sql-zoo-long-practice-main/lib/06_joins_warmup.rb
@@ -36,12 +36,24 @@ end
 def films_from_sixty_two
   # List the films where the yr is 1962. Show id, title.
   execute(<<-SQL)
+  SELECT 
+  id, 
+  title 
+  from 
+  movies 
+  where 
+  yr = 1962
   SQL
 end
 
 def year_of_kane
   # Give the yr of 'Citizen Kane'.
   execute(<<-SQL)
+  select 
+  yr 
+  from movies 
+  where 
+  title = 'Citizen Kane'
   SQL
 end
 
@@ -50,24 +62,46 @@ def trek_films
   # these movies include the words 'Star Trek' in the title). Order results by
   # year.
   execute(<<-SQL)
+  SELECT 
+  id, 
+  title, 
+  yr
+  from 
+  movies 
+  where 
+  title LIKE '%Star Trek%'
+  order by yr 
   SQL
 end
 
 def films_by_id
   # What are the titles of the films with id 1119, 1595, 1768?
   execute(<<-SQL)
+  select title 
+  from movies 
+  where id = 1119
+  or id = 1595
+  or id = 1768
   SQL
 end
 
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+  select id 
+  from 
+  actors 
+  where 
+  name = 'Glenn Close'
   SQL
 end
 
 def casablanca_id
   # What is the id of the film 'Casablanca'?
   execute(<<-SQL)
+  select id 
+  from movies 
+  where title = 'Casablanca'
   SQL
 end
 
@@ -75,11 +109,23 @@ def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
   execute(<<-SQL)
+  select name 
+  from 
+  actors 
+  join castings on actors.id = castings.actor_id
+  join movies on castings.movie_id = movies.id
+  where title = 'Casablanca' 
   SQL
 end
 
 def alien_cast
   # Obtain the cast list for the film 'Alien'.
   execute(<<-SQL)
+  select name 
+  from 
+  actors 
+  join castings on actors.id = castings.actor_id
+  join movies on castings.movie_id = movies.id
+  where title = 'Alien' 
   SQL
 end
